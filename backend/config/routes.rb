9254990240e93637c61993a5bd480af41f568ce8
Philipe <philipe-leandro@hotmail.com>
2022-07-29 17:22:54 -0300

    Add omniauth configs Co-authored-by: Davide Almeida
    <davide.jhonatan@gmail.com>

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -40,7 +40,9 @@ Rails.application.routes.draw do
   get '/api/v1/seed_database', to: 'api/v1/api#seed_database'
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
