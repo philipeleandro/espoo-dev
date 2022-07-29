345ab79345b880db46d483898250ab178b2b7001
Philipe <philipe-leandro@hotmail.com>
2022-07-29 17:23:31 -0300

    Add method in user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,9 +6,11 @@
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  phone                  :string
+#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  uid                    :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  role_id                :bigint           not null
@@ -22,7 +24,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+         :omniauthable, :jwt_authenticatable,
+         jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null,
+         omniauth_providers: [:google_oauth2]
 
   has_many :answers_surveys, dependent: :destroy
   has_many :options, dependent: :destroy
@@ -36,4 +40,11 @@ class User < ApplicationRecord
   delegate :admin?, to: :role
   delegate :teacher?, to: :role
   delegate :student?, to: :role
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
