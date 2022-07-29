4d61b2281a06c7faaf03cc45f88a879b5a2c6a91
Philipe <philipe-leandro@hotmail.com>
2022-07-29 17:21:23 -0300

    Add providers and uid to user
    Co-authored-by: Davide Almeida <davide-almeida@users.noreply.github.com>

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_21_133302) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_200029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,7 +58,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_21_133302) do
     t.datetime "updated_at", null: false
     t.integer "group_dependency_id"
     t.integer "trail_id"
+    t.index ["group_dependency_id"], name: "index_groups_on_group_dependency_id"
     t.index ["name", "user_id"], name: "index_groups_on_name_and_user_id", unique: true
+    t.index ["trail_id"], name: "index_groups_on_trail_id"
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
@@ -146,6 +148,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_21_133302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "role_id", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
